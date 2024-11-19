7cf612226e1c6e3030f576269331518f1e802af4
Ahmet Hakan Yıldız <129862074+yildizahmethakan0@users.noreply.github.com>
2024-11-19 10:05:44 +0300

    Update main.py

--- JSONpassword/main.py
+++ JSONpassword/main.py
@@ -4,9 +4,7 @@ from random import choice, randint, shuffle
 import pyperclip
 import json
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#Password Generator Project
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -23,7 +21,7 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+
 def save():
 
     website = website_entry.get()
